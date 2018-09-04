ff6ececa7cd11b7866d58c2fcaa0a0c86db292c6
Chanjoo Lee <lees3301@gmail.com>
2018-09-04 22:18:38 +0900

    Added Main functions - attached camera views, buttons - initializing TensorFlow model

--- app/src/main/java/com/tistory/dayglo/showmewhatigot/MainActivity.java
+++ app/src/main/java/com/tistory/dayglo/showmewhatigot/MainActivity.java
@@ -1,12 +1,25 @@
 package com.tistory.dayglo.showmewhatigot;
 
+import android.graphics.Bitmap;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.tistory.dayglo.showmewhatigot.tflite.Classifier;
+import com.tistory.dayglo.showmewhatigot.tflite.TensorFlowImageClassifier;
+import com.wonderkiln.camerakit.CameraKitError;
+import com.wonderkiln.camerakit.CameraKitEvent;
+import com.wonderkiln.camerakit.CameraKitEventListener;
+import com.wonderkiln.camerakit.CameraKitImage;
+import com.wonderkiln.camerakit.CameraKitVideo;
 import com.wonderkiln.camerakit.CameraView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
     private static final String MODEL_PATH = "optimized_graph.lite";
@@ -14,15 +27,87 @@ public class MainActivity extends AppCompatActivity {
     private static final int INPUT_SIZE = 224;
 
     private Classifier classifier;
+    private Executor executor = Executors.newSingleThreadExecutor();
 
     private TextView textViewResult;
     private FloatingActionButton btnDetectObject, btnToggleCamera;
     private CameraView cameraView;
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        cameraView.stop();
+        super.onPause();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cameraView = findViewById(R.id.cameraView);
+        textViewResult = findViewById(R.id.textViewResult);
+
+        btnToggleCamera = findViewById(R.id.btnToggleCamera);
+        btnDetectObject = findViewById(R.id.btnDetectObject);
+
+        cameraView.addCameraKitListener(new CameraKitEventListener() {
+            @Override
+            public void onEvent(CameraKitEvent cameraKitEvent) {}
+
+            @Override
+            public void onError(CameraKitError cameraKitError) {}
+
+            @Override
+            public void onImage(CameraKitImage cameraKitImage) {
+                Bitmap bitmap = cameraKitImage.getBitmap();
+                bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
+                final List<Classifier.Recognition> results = classifier.recognizeImage(bitmap); // 결과 얻어오기
+
+                textViewResult.setText(results.toString());
+
+            }
+
+            @Override
+            public void onVideo(CameraKitVideo cameraKitVideo) {}
+        });
+
+        btnToggleCamera.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                cameraView.toggleFacing();
+            }
+        });
+
+        btnDetectObject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cameraView.captureImage();
+            }
+        });
+
+
+        initTensorFlowAndLoadModel(); // initailizing model
+    }
+
+    private void initTensorFlowAndLoadModel() {
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    classifier = TensorFlowImageClassifier.create(getAssets(),
+                            MODEL_PATH, LABEL_PATH, INPUT_SIZE);
+
+                } catch (IOException e) {
+                    throw new RuntimeException("Error initializing TensorFlow", e);
+                }
+            }
+        });
     }
 }
