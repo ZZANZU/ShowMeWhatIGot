79ab38e7dec692b79966d1c8a35fc0428d0f5b9e
Chanjoo Lee <lees3301@gmail.com>
2018-09-04 21:54:51 +0900

    Added Classifier - image recognition - returns probability

--- app/src/main/java/com/tistory/dayglo/showmewhatigot/tflite/TensorFlowImageClassifier.java
+++ app/src/main/java/com/tistory/dayglo/showmewhatigot/tflite/TensorFlowImageClassifier.java
@@ -1,10 +1,23 @@
 package com.tistory.dayglo.showmewhatigot.tflite;
 
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 
 import org.tensorflow.lite.Interpreter;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class TensorFlowImageClassifier implements Classifier {
     private static final int MAX_RESULTS = 3;
@@ -15,17 +28,106 @@ public class TensorFlowImageClassifier implements Classifier {
     private static final int IMAGE_MEAN = 128;
     private static final float IMAGE_STD = 128.0f;
 
-    private Interpreter interpreter;
+    private Interpreter interpreter; // tflite에서 제공
     private int inputSize;
     private List<String> labelList;
 
     @Override
     public List<Recognition> recognizeImage(Bitmap bitmap) {
-        return null;
+        ByteBuffer byteBuffer = convertBitmapToByteBuffer(bitmap);
+        float[][] result = new float[1][labelList.size()];
+
+        interpreter.run(byteBuffer, result);
+
+        return getSortedResult(result);
+    }
+
+    private List<Recognition> getSortedResult(float[][] labelProbArray) {
+        PriorityQueue<Recognition> pq = new PriorityQueue<>(MAX_RESULTS,
+                new Comparator<Recognition>() {
+                    @Override
+                    public int compare(Recognition lhs, Recognition rhs) {
+                        return Float.compare(rhs.getConfidence(), lhs.getConfidence());
+                    }
+                });
+
+        for (int i = 0; i < labelList.size(); ++i) {
+            // confidence : 확신, detection percentage
+            float confidence = (labelProbArray[0][i] * 100) / 127.0f; //  & 0xff 삭제해봄, 임시방편으로 100 곱하니까 퍼센트값 잘 나옴ㅠㅠ
+
+            // 0.1(10%) 이상이면 통과, 출력 준비
+            if (confidence > THRESHOLD) {
+                pq.add(new Recognition("" + i,
+                        labelList.size() > i ? labelList.get(i) : "unknown",
+                        confidence));
+            }
+        }
+
+        final ArrayList<Recognition> recognitions = new ArrayList<>();
+        int recognitionsSize = Math.min(pq.size(), MAX_RESULTS);
+        for (int i = 0; i < recognitionsSize; ++i) {
+            recognitions.add(pq.poll());
+        }
+
+        return recognitions;
     }
 
     @Override
     public void close() {
+        interpreter.close();
+        interpreter = null;
+    }
 
+    private ByteBuffer convertBitmapToByteBuffer(Bitmap bitmap) {
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * BATCH_SIZE * inputSize * inputSize * PIXEL_SIZE);
+        byteBuffer.order(ByteOrder.nativeOrder());
+
+        int[] intValues = new int[inputSize * inputSize];
+        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        int pixel = 0;
+        for (int i = 0; i < inputSize; ++i) {
+            for (int j = 0; j < inputSize; ++j) {
+                final int val = intValues[pixel++];
+                byteBuffer.putFloat((((val >> 16) & 0xFF)-IMAGE_MEAN)/IMAGE_STD);
+                byteBuffer.putFloat((((val >> 8) & 0xFF)-IMAGE_MEAN)/IMAGE_STD);
+                byteBuffer.putFloat((((val) & 0xFF)-IMAGE_MEAN)/IMAGE_STD);
+            }
+        }
+        return byteBuffer;
+    }
+
+
+    private MappedByteBuffer loadModelFile(AssetManager assetManager, String modelPath) throws IOException {
+        AssetFileDescriptor fileDescriptor = assetManager.openFd(modelPath);
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+    }
+
+    private List<String> loadLabelList(AssetManager assetManager, String labelPath) throws IOException {
+        List<String> labelList = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(assetManager.open(labelPath)));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            labelList.add(line);
+        }
+        reader.close();
+        return labelList;
+    }
+
+    public static Classifier create(AssetManager assetManager,
+                                    String modelPath,
+                                    String labelPath,
+                                    int inputSize) throws IOException {
+        TensorFlowImageClassifier tfClassifier = new TensorFlowImageClassifier();
+
+        tfClassifier.interpreter = new Interpreter(tfClassifier.loadModelFile(assetManager, modelPath));
+        tfClassifier.labelList = tfClassifier.loadLabelList(assetManager, labelPath);
+        tfClassifier.inputSize = inputSize;
+
+        return tfClassifier;
     }
 }
